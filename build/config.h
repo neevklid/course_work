dfbde4ac4e27d029928e616593b23aac72dd9fe5
Анастасия Козина <m1903531@edu.misis.ru>
2021-01-22 18:39:18 +0300

    deleted build dirs

--- build/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H_IN
-#define CONFIG_H_IN
-
-#define CL_PATH "C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x64/cl.exe"
-
-#endif // CONFIG_H_IN
